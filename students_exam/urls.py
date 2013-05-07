583bb9e10e2e06b14028bda4fcb205085a0a6771
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2013-05-07 09:21:16 +0200

    Static and media on url

--- students_exam/urls.py
+++ students_exam/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from students_exam import settings
+
 
 admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     (r'^grappelli/', include('grappelli.urls')),
     url(r'^api/', include('exams.urls')),
+)
+
+# Static files
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
